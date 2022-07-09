9338c31efeeac40ccc0f85a2e2495cac4a449d22
Ollie Cheng <olliecheng@outlook.com>
2022-07-09 11:19:30 +0800

    Change baseURL

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -14,6 +14,7 @@
 			padding: 0;
 		}
 	</style>
+	<base href="/papers" />
 </svelte:head>
 
 <Sidebar />
--- svelte.config.js
+++ svelte.config.js
@@ -9,11 +9,13 @@ const config = {
 
 	kit: {
 		adapter: adapter(),
-
+		paths: {
+			base: '/papers'
+		},
 		prerender: {
-      // This can be false if you're using a fallback (i.e. SPA mode)
-      default: true
-    }
+			// This can be false if you're using a fallback (i.e. SPA mode)
+			default: true
+		}
 	}
 };
 
